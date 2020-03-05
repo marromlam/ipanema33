5e70cf3deb1f002e65c223d3500356c204777c0f
Marcos Romero Lamas <marcos.romero.lamas@cern.ch>
2020-03-05 13:08:21 +0100

    Update parameters_test to load ipanema instead of Parameters, Parameter

--- examples/parameters_test.py
+++ examples/parameters_test.py
@@ -1,19 +1,19 @@
 """
 
-Test Parameters
+Test ipanema.Parameters
 
 """
 
 
-from ipanema import Parameters, Parameter
+import ipanema
 
-a1 = Parameter('a1',11,stdev=0.1,min=7,max=12.3,latex='a_1')
-a2 = Parameter('a2',0.2,stdev=0.1,min=-0.5,max=1.3,latex='a_2')
-b  = Parameter('b',201.1,stdev=0.1,min=1,max=20.3,latex='b')
+a1 = ipanema.Parameter('a1',11,stdev=0.1,min=7,max=12.3,latex='a_1')
+a2 = ipanema.Parameter('a2',0.2,stdev=0.1,min=-0.5,max=1.3,latex='a_2')
+b  = ipanema.Parameter('b',201.1,stdev=0.1,min=1,max=20.3,latex='b')
 
-c1 = Parameter('c1',11,stdev=0.1,min=7,max=12.3,latex='c_1')
-c2 = Parameter('c2',0.2,stdev=0.1,min=-0.5,max=1.3,latex='c_2')
-d  = Parameter('d',101.1,stdev=0.1,min=1,max=20.3,latex='b')
+c1 = ipanema.Parameter('c1',11,stdev=0.1,min=7,max=12.3,latex='c_1')
+c2 = ipanema.Parameter('c2',0.2,stdev=0.1,min=-0.5,max=1.3,latex='c_2')
+d  = ipanema.Parameter('d',101.1,stdev=0.1,min=1,max=20.3,latex='b')
 
 # One can operate on these parameters
 print(f"a1 + a2 = {a1+a2}")
@@ -32,15 +32,15 @@ a2.set(free = False)
 print(f"a1 = {a1}")
 print(f"a2 = {a2}")
 
-# Create a Parameters dictionary
-A = Parameters()
-B = Parameters()
+# Create a ipanema.Parameters dictionary
+A = ipanema.Parameters()
+B = ipanema.Parameters()
 A.add(a1,a2,b)
 B.add(c1,c2,d)
 A.print()
 B.print()
 
-# Merge Parameters dictionary
+# Merge ipanema.Parameters dictionary
 C = A+B
 A.print()
 B.print()
