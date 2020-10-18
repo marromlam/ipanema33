33f69eca8c719ca02f061e9d4c2c9c4b562a5f2a
Marcos Romero Lamas <marcos.romero.lamas@cern.ch>
2020-10-18 17:23:59 +0200

    some index testin

--- examples/dataset_test.py
+++ examples/dataset_test.py
@@ -1,6 +1,7 @@
 from ipanema import Sample
 from ipanema.samples import DataSet
 import numpy as np
+import pandas as pd
 
 x = np.array([1,2,3,4,5,6])
 y = 2*np.array([1,2,3,4,5,6])
@@ -9,6 +10,27 @@ y = 2*np.array([1,2,3,4,5,6])
 
 s1 = Sample.from_numpy({'x':x, 'y':y},name='s1')
 s2 = Sample.from_numpy({'x':x, 'y':y},name='s2')
+
+
+
+s11 = s1.df.query('x==2')
+s12 = s1.df.query('x!=2')
+pd.concat([s12,s11])
+
+i11 = list(s11.index)
+i12 = list(s12.index)
+
+X = np.zeros_like(i11+i12)
+X[i11] = s11['x']
+X[i12] = s12['x']
+X
+
+
+
+
+
+
+
 
 
 
