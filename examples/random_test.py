12faf1ae2991251579ee0ebf0c40c94ed0406db5
Marcos Romero Lamas <marcos.romero.lamas@cern.ch>
2020-11-30 13:49:41 +0100

    add random test script

---
+++ examples/random_test.py
@@ -0,0 +1,69 @@
+import ipanema
+
+from reikna.cluda import functions, dtypes
+import numpy as np
+from scipy.special import wofz
+import matplotlib.pyplot as plt
+ipanema.initialize('cuda',1)
+
+import badjanak
+
+
+
+
+
+
+
+
+
+
+#%% ----------------------------------------------------------------------------
+import ipanema
+
+from reikna.cluda import functions, dtypes
+import numpy as np
+from scipy.special import wofz
+import matplotlib.pyplot as plt
+ipanema.initialize('opencl',1)
+
+#%% ---
+
+prog = THREAD.compile(
+"""
+#define USE_DOUBLE ${USE_DOUBLE}
+
+#include <ipanema/random.cpp>
+#include <ipanema/complex.cpp>
+#include <ipanema/special.cpp>
+
+KERNEL
+void generate( GLOBAL_MEM double *out, int seed)
+{
+  int idx = get_global_id(0);
+  int _seed = seed+idx;
+  out[idx] = rngLogNormal( 80.0, 15.0, &_seed, 100 ) ;
+}
+
+""",
+compiler_options=[f"-I{ipanema.IPANEMALIB}"], render_kwds={"USE_DOUBLE":"1"},keep=False)
+
+
+
+#%% ----
+
+a
+a = ipanema.ristra.allocate(np.float64(100000*[0]))
+prog.generate(a, np.int32(324234324324), global_size=(len(a),))
+plt.hist(a.get())
+
+x = np.random.rand(int(1e5))
+y = np.random.rand(int(1e5))
+
+z = THREAD.to_device(np.complex128([x+1j*y]))
+w = THREAD.to_device(np.complex128([x+1j*y]))
+
+#%%
+
+%time wofz(x+1j*y)
+
+%time ipanema.ristra.get(prog.pywofz(z,w,global_size=(len(z),)))
