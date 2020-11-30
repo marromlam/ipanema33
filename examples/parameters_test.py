5edf4cf93a176b4fd2d75fbfee0aaa9a56e92918
Marcos Romero Lamas <marcos.romero.lamas@cern.ch>
2020-11-30 13:36:18 +0100

    add cov test to parameter_test

--- examples/parameters_test.py
+++ examples/parameters_test.py
@@ -3,6 +3,36 @@
 Test ipanema.Parameters
 
 """
+import numpy as np
+import ipanema
+
+example_corr = np.array([[1,0.1,0.3],[0.3,1,0.1],[0.4,0.8,1]])
+example_corr
+
+corr_a = { p:example_corr[0,i] for i,p in enumerate(['a','b','c'])}
+corr_b = { p:example_corr[1,i] for i,p in enumerate(['a','b','c'])}
+corr_c = { p:example_corr[2,i] for i,p in enumerate(['a','b','c'])}
+
+corr_a
+corr_b
+corr_c
+
+a = ipanema.Parameter('a',11,stdev=0.1,min=7,max=12.3,latex='a',correl=corr_a)
+b = ipanema.Parameter('b',0.2,stdev=0.3,min=-0.5,max=1.3,latex='b',correl=corr_b)
+c  = ipanema.Parameter('c',20.1,stdev=2,min=1,max=20.3,latex='c',correl=corr_c)
+
+
+pars = ipanema.Parameters()
+pars.add(a,b,c)
+pars.correl_mat()
+pars.cov()
+
+np.array(pars)
+
+import uncertainties as unc
+unc.correlated_values(np.array(pars), pars.cov())
+
+
 
 
 import ipanema
