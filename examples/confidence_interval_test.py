74520fe0fde6ce76bf9442581ff5bd0da61123a2
Marcos Romero Lamas <marcos.romero.lamas@cern.ch>
2020-11-30 13:35:37 +0100

    some more tests in ci tester

--- examples/confidence_interval_test.py
+++ examples/confidence_interval_test.py
@@ -1,11 +1,16 @@
 
+
+
+
+
+
 #%%
 
 import numpy as np
 import ipanema
 import matplotlib.pyplot as plt
 
-
+1+1
 
 def residual(pars,x,y=None):
   p = pars.valuesdict()
@@ -26,8 +31,8 @@ y = np.array([0.37,0.58,0.83,1.15,1.36,1.62,1.90,2.18,2.45])
 plt.plot(x,y,'.')
 
 
-help(mini.emcee)
-res_emcee = mini.optimize(method='emcee', steps=100000, is_weighted=False)
+help(lmmini.emcee)
+res_emcee = lmmini.optimize(method='emcee', steps=100000, is_weighted=False)
 res_emcee.params.print()
 
 import corner
@@ -150,130 +155,318 @@ unc.correlated_values(popt,pcov)
 
 
 
+#%% TEST 2 #####################################################################
 
-
-
-
-
-
-
-
-
-
-
-ci, trace = ipanema.confidence_interval(mini, res_bfgs)
-
-
-
-
-
-
-ci['a'][-1]-ci['a'][0], res_bfgs.params['a'].stdev
-ci['a'][+1]-ci['a'][0], res_bfgs.params['a'].stdev
-ci['b'][-1]-ci['b'][0], res_bfgs.params['b'].stdev
-ci['b'][+1]-ci['b'][0], res_bfgs.params['b'].stdev
-
-
-
-
-
-
-
-
-
-
-ci['a2'][-1]-ci['a2'][0], res_minuit.params['a2'].stdev
-ci['a2'][+1]-ci['a2'][0], res_minuit.params['a2'].stdev
-
-ci['t2'][-1]-ci['t2'][0], res_minuit.params['t2'].stdev
-ci['t2'][+1]-ci['t2'][0], res_minuit.params['t2'].stdev
-
-
-
-
-#%%
-plt.close()
-nfree = len(trace.keys())
-fig, axes = plt.subplots(figsize=(11, 10), ncols=nfree, nrows=nfree,  sharex='col', sharey='row')
-for i in range(0,nfree):
-  for j in range(0,nfree):
-    #if i<j:
-    #  axes[i, j].axis('off')
-    0
-
-
-
-for i, k1 in enumerate(trace.keys()):
-  for j, k2 in enumerate(trace[k1].keys()):
-      if (k2 != 'prob'):# & (k2 != k1):
-        x,y,p = trace[k1][k1], trace[k1][k2], trace[k1]['prob']
-        axes[j,i].scatter(x, y, c=p)
-        if j+1 == len(trace.keys()):
-          axes[j,i].set_xlabel(k1)
-        if i == 0:
-          axes[j,i].set_ylabel(k2)
-plt.show()
-
-"""
-fig, axes = plt.subplots(1, 2, figsize=(12.8, 4.8))
-cx1, cy1, prob  = trace['a1']['a1'], trace['a1']['t2'], trace['a1']['prob']
-cx2, cy2, prob2 = trace['t2']['t2'], trace['t2']['a1'], trace['t2']['prob']
-
-axes[0].scatter(cx1, cy1, c=prob)
-axes[0].set_xlabel('a1')
-axes[0].set_ylabel('t2')
-
-axes[1].scatter(cx2, cy2, c=prob2)
-axes[1].set_xlabel('t2')
-axes[1].set_ylabel('a1')
-"""
-
-
-
-# plot confidence intervals (a1 vs t2 and a2 vs t2)
-fig, axes = plt.subplots(1, 2, figsize=(12.8, 4.8))
-cx, cy, grid = ipanema.confidence_interval2d(mini, result2, 'a1', 't2', 30, 30)
-ctp = axes[0].contourf(cx, cy, grid, 3, colors=['C1','C3','C0','white'], alpha=0.5)
-#fig.colorbar(ctp, ax=axes[0])
-axes[0].set_xlabel('a1')
-axes[0].set_ylabel('t2')
-
-cx, cy, grid = ipanema.confidence_interval2d(mini, result2, 'a2', 't2', 30, 30)
-ctp = axes[1].contourf(cx, cy, grid, 3, colors=['C1','C3','C0','white'], alpha=0.5)
-#fig.colorbar(ctp, ax=axes[1])
-axes[1].set_xlabel('a2')
-axes[1].set_ylabel('t2')
-
-
-
-nfree = len(trace.keys())
-fig, axes = plt.subplots(figsize=(11, 10), ncols=nfree, nrows=nfree,  sharex='col', sharey='row')
-for i in range(0,nfree):
-  for j in range(0,nfree):
-    if i<=j:
-      axes[i, j].axis('off')
-
-
-
-for i, k1 in enumerate(result2.params.keys()):
-  for j, k2 in enumerate(result2.params.keys()):
-    if i < j:
-      if (k2 != 'prob'):# & (k2 != k1):
-        x,y,z = ipanema.confidence_interval2d(mini, result2, k1, k2, 30, 30)
-        axes[j,i].contourf(x, y, z, 3, colors=['C1','C3','C0','white'], alpha=0.5)
-        if j+1 == len(trace.keys()):
-          axes[j,i].set_xlabel(k1)
-        if i == 0:
-          axes[j,i].set_ylabel(k2)
-plt.show()
-
-
-
-
-
-
-result2 = mini.optimize(method='emcee', params = result1.params, steps=5000)
+import matplotlib.pyplot as plt
+import numpy as np
+import ipanema
+import lmfit
+from ipanema import confidence_interval, confidence_interval2d, Parameters, ristra
 import corner
+from tqdm import tqdm
+import uncertainties as unc
+
+x = np.linspace(1, 10, 250)
+np.random.seed(0)
+y = 3.0*np.exp(-x/2) - 5.0*np.exp(-(x-0.1)/10.) + 0.1*np.random.randn(x.size)
+
+# parameters
+pars = ipanema.Parameters()
+pars.add({'name':'a1', 'value':4, 'latex':"a_1"})
+pars.add({'name':'a2', 'value':4, 'latex':"a_2"})
+pars.add({'name':'t1', 'value':3, 'latex':"t_1"})
+pars.add({'name':'t2', 'value':3, 'latex':"t_2"})
+
+# residual function
+def residual(p):
+  d = p.valuesdict()
+  return d['a1']*np.exp(-x/d['t1']) + d['a2']*np.exp(-(x-0.1)/d['t2']) - y
+
+# create Optimizer
+ipmini = ipanema.Optimizer(residual, pars, residual_reduce='chi2')
+
+# first solve with Nelder-Mead algorithm
+ipout1 = ipmini.optimize(method='nelder', verbose=False)
+ipout1 = ipmini.optimize(method='emcee', verbose=False, params=ipout1.params)
+print(ipout1)
+#fig, ax = plot_contours(ipmini, ipout1, size=(20,20))
 
 
-corner.corner(result2.flatchain)
+
+dir(ipout1)
+
+
+print(ipout1.params)
+np.array(ipout1.params)
+ipout1.params.valuesdict()
+print(ipout1.params.dumps())
+
+ipout1.params.uvaluesdict()['a1'].s
+[result.params['f_s'].value,result.params['f_s'].stdev, result.chi2red ]
+
+
+ipout1.params['a1'].uvalue
+ipout1.params['a1'].uvalue**2
+
+ipout1.params.corr()
+ipout1.params.cov()
+
+
+print(ipout1.params.dump_latex(cols=['value','stdev','free'], caption='soy la polla'))
+
+#%% shit
+
+
+def plot_contours(mini, result, params=False, size=(20,20)):
+  # look for free parameters
+  if params:
+    _params = params
+  else:
+    _params = list(result.params.keys())
+  params = []
+  for p in _params:
+    if result.params[p].free:
+      params.append(p)
+    else:
+      print(" WARNING: ")
+
+  nfree = sum([1 if result.params[p].free else 0 for p in params])
+  print(f"ipanema is about to run ({size[0]}x{size[1]})x{int(nfree*(nfree-1)/2)} fits\n")
+
+  fig, axes = plt.subplots(figsize=(10*nfree//2, 10*nfree//2), ncols=nfree, nrows=nfree)#, sharex='col', sharey='row')
+
+  for i in range(0,nfree):
+    for j in range(0,nfree):
+      if i<j:
+        axes[i, j].axis('off')
+
+  with tqdm(total=int(nfree+nfree*(nfree-1)/2)) as pbar:
+    for i, k1 in enumerate(params):
+      for j, k2 in enumerate(params):
+        if i < j:
+          x,y,z = confidence_interval2d(mini, result, k1, k2, size[0], size[1])
+          #axes[j, i].contourf(x, y, z, np.linspace(0, 1, 11), cmap='GnBu')
+          axes[j, i].contourf(x, y, z, [0, 1-np.exp(-0.5), 1-np.exp(-2.0), 1-np.exp(-4.5)], cmap='GnBu')#, colors=['C1','C3','C2','C4'], alpha=0.5)
+          if j+1 == nfree:
+            axes[j,i].set_xlabel(f"${result.params[k1].latex}$")
+          if i == 0:
+            axes[j,i].set_ylabel(f"${result.params[k2].latex}$")
+          axes[j,i].set_title(f'[{i},{j}]')
+          pbar.update(1)
+      #x,y,z = confidence_interval2d(mini, result, k1, k2, 30, 30)
+      if i == nfree-1:
+        axes[i,i].plot(y, np.sum(z,0), 'k')
+        #axes[i,i].get_shared_y_axes().remove(axes[nfree-1,j])
+        #axes[i,i].get_shared_y_axes().remove(axes[nfree-1,j])
+      else:
+        axes[i,i].plot(x,np.sum(z,1), 'k')
+        #swap(axes[i,i])
+
+      pbar.update(1)
+  ci, trace = confidence_interval(mini, result, params)
+  for i, k1 in enumerate(params):
+    for j, k2 in enumerate(params):
+      print(k1,k2)
+      if i <= j:
+        for color, x in enumerate(ci[k1].values()):
+          print(color, x, ci[k1][-1], ci[k1][+1])
+          axes[j,i].axvline( x=x, color=f'k', linestyle=':', alpha=0.5)
+          _var_lo = unc.ufloat(result.params[k2].value, abs(ci[k2][-1]-ci[k2][0]))
+          _var_hi = unc.ufloat(result.params[k2].value, abs(ci[k2][+1]-ci[k2][0]))
+          _v = f"{_var_lo:.2uL}".split('\pm')[0]
+          _l = f"{_var_lo:.2uL}".split('\pm')[1]
+          _u = f"{_var_hi:.2uL}".split('\pm')[1]
+          _tex = result.params[k2].latex
+          _p = f"parab $\pm {result.params[k2].unc_round[1]}$"
+      if i==j:
+        axes[j,i].set_title(f'${_tex} = {_v}_{{-{_l}}}^{{+{_u}}}$ ({_p})')
+
+  return fig, axes
+
+
+
+fig, ax = plot_contours(ipmini, ipout1, size=(30,30))
+fig.savefig('test_contours.pdf')
+
+
+def romeroopt(params):
+  % Finding root
+    for l = 1:1:maxit
+      Jf0 = NumJacobian(f,x0);                          % Jacobian of f at x0
+      root = Jf0\(-f(x0)) + x0;      % RELAXED, non relaxed was x0-J^-1*f(x0)
+      froot=f(root);
+      if norm(root-x0,2) < eepsi || norm(froot,2) < edelt
+        break;
+      end
+      x0 = root;
+    end
+    if l == maxit
+      warning('Max number of iterations.');
+    end
+
+
+
+
+
+shit = ipanema.Sample.from_root('/scratch17/marcos.romero/sidecar/2015/MC_Bs2JpsiPhi/v0r5.root')
+plt.hist(shit.df.eval('sw').values)
+plt.hist(shit.df.eval('sw/gb_weights').values)
+
+
+def f(p,x):
+  return p[0]+np.cos(p[1])
+pars = [1,2]
+
+X = np.linspace(-3,3,100)
+plt.plot(X, -X*np.sin(pars[1]*X))
+
+
+numericJacobian(f, X, pars, f_size = 1)
+
+
+fastjac( f, pars, x=X[0])
+
+plt.plot(X, -pars[1]*np.sin(pars[1]*X))
+fastjac( f, pars)
+f(pars)
+
+
+def fastjac( f, x0, r=False, c=False, *args, **kwargs ):
+  print(args, kwargs)
+  if not r or c:
+    r,c = np.atleast_2d(f(x0, *args, **kwargs)).shape
+    c = max(r,c); r = max(r,len(x0))
+  J = np.zeros((r,c))
+  for l, xl in enumerate(x0):
+    if xl != 0:
+      h = np.sqrt(np.finfo(float).eps)*x0[l]
+    else:
+      h = 1e-14
+    xh1 = np.copy(x0); xh1[l] = x0[l] + h
+    xh2 = np.copy(x0); xh2[l] = x0[l] - h
+    J[l,:] = (f(xh1, *args, **kwargs) - f(xh2, *args, **kwargs)) / (2*h)
+
+  return J.T
+
+
+def numericJacobian(f, x, vals, f_size = 1):
+  J = np.zeros([len(x), f_size, len(vals)])
+  for l in range(0,len(vals)):
+    if vals[l]!= 0:    h = np.sqrt(np.finfo(float).eps)*vals[l];
+    else:           h = 1e-14;
+    vals1 = np.copy(vals); vals1[l] += +h
+    vals2 = np.copy(vals); vals2[l] += -h;
+    f1 = f(x,*vals1).astype(np.float64)
+    f2 = f(x,*vals2).astype(np.float64)
+    thisJ = ((f(x,*vals1) - f(x,*vals2))/(2*h)).astype(np.float64)
+    J[:,0,l] = thisJ # nowadays only scalar f allowed
+  return J.T
+
+#%% -----
+
+
+
+
+
+
+
+fit_emcee = ipmini.optimize(method='emcee', params=ipout1.params, is_weighted=False)
+print(fit_emcee)
+import corner
+corner.corner(fit_emcee.flatchain, labels=[f"${p.latex}$" for p in fit_emcee.params.values()], quantiles=[0.16, 0.5, 0.84], show_titles=True)
+
+print(fit_emcee.params)
+fit_emcee.params.build( fit_emcee.params, ['a1','a2']).cov()
+
+
+
+fit_emcee.flatchain
+
+
+import lmfit
+p = lmfit.Parameters()
+p.add_many(('a1', 4.), ('a2', 4.), ('t1', 3.), ('t2', 3., True))
+mi = lmfit.minimize(residual, p, method='nelder', nan_policy='omit')
+lmfit.printfuncs.report_fit(mi.params, min_correl=0.5)
+print(mi)
+mi.params.add('__lnsigma', value=np.log(0.1), min=np.log(0.001), max=np.log(2))
+fit_emcee = lmfit.minimize(residual, method='emcee', nan_policy='omit', steps=10000, #burn=300,  thin=20,
+                     params=mi.params, is_weighted=False, progress=True)
+fit_emcee
+import corner
+corner.corner(fit_emcee.flatchain)
+
+# gradient
+ipout3 = ipmini.optimize(method='emcee', params=ipout1.params, is_weighted=False)
+print(ipout3)
+
+ipout2 = ipmini.optimize(method='leastsq', params=ipout1.params)
+print(ipout2)
+
+
+#%% ---
+
+result = ipmini.optimize(method='emcee', params = ipout1.params, steps=5000)
+corner.corner(result.flatchain)
+
+
+from ipanema import confidence_interval2d
+import tqdm
+nfree
+def plot_contours(mini, result, params=False, size=(20,20)):
+  # look for free parameters
+  if not params:
+    params = list(result.params.keys())
+  nfree = sum([1 if result.params[p].free else 0 for p in params])
+  axes = nfree*[nfree*[]]
+
+  with tqdm(total=int(nfree+nfree*(nfree-1)/2)) as pbar:
+    for i, k1 in enumerate(params):
+      for j, k2 in enumerate(params):
+        if i < j:
+          x,y,z = confidence_interval2d(mini, result, k1, k2, size[0], size[1])
+          axes[j, i].contourf(x, y, z, np.linspace(0, 1, 11), cmap='GnBu')# 4, colors=['C1','C2','C3','C4','white'])
+          if j+1 == nfree:
+            axes[j,i].set_xlabel(f"${result.params[k1].latex}$")
+          if i == 0:
+            axes[j,i].set_ylabel(f"${result.params[k2].latex}$")
+          axes[j,i].set_title(f'[{i},{j}]')
+          pbar.update(1)
+      #x,y,z = confidence_interval2d(mini, result, k1, k2, 30, 30)
+      if i == nfree-1:
+        axes[i,i].plot(y, np.sum(z,0))
+        #axes[i,i].get_shared_y_axes().remove(axes[nfree-1,j])
+        #axes[i,i].get_shared_y_axes().remove(axes[nfree-1,j])
+      else:
+        axes[i,i].plot(x,np.sum(z,1))
+        #swap(axes[i,i])
+      axes[i,i].set_title(f'[{i},{i}]')
+      pbar.update(1)
+  return fig, axes
+
+
+
+
+
+def swap(*line_list):
+    """
+    Example
+    -------
+    line = plot(linspace(0, 2, 10), rand(10))
+    swap(line)
+    """
+    for lines in line_list:
+        try:
+            iter(lines)
+        except:
+            lines = [lines]
+
+        for line in lines:
+            xdata, ydata = line.get_xdata(), line.get_ydata()
+            line.set_xdata(ydata)
+            line.set_ydata(xdata)
+            line.axes.autoscale_view()
+line = plt.plot(np.linspace(0, 2, 10), np.random.rand(10))
+swap(line)
+
+# %%
