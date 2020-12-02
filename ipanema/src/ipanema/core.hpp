4c628d478e5bd4b759e543a4a728c541ad88dc90
Marcos Romero Lamas <marcos.romero.lamas@cern.ch>
2020-12-02 17:14:13 +0100

    add atomicAdd for opencl

--- ipanema/src/ipanema/core.hpp
+++ ipanema/src/ipanema/core.hpp
@@ -1,7 +1,24 @@
 #ifdef USE_DOUBLE
-#define ftype double
-#define ctype double2
+  #define ftype double
+  #define ctype double2
+  #ifndef CUDA
+  #pragma OPENCL EXTENSION cl_khr_int64_base_atomics: enable
+    WITHIN_KERNEL
+    void atomicAdd(volatile __global double *addr, double val)
+    {
+      union {
+        long u;
+        double f;
+      } next, expected, current;
+      current.f = *addr;
+      do {
+        expected.f = current.f;
+        next.f = expected.f + val;
+        current.u = atomic_cmpxchg( (volatile __global long *) addr, expected.u, next.u);
+      } while( current.u != expected.u );
+    }
+  #endif
 #else
-#define ftype float
-#define ctype float2
+  #define ftype float
+  #define ctype float2
 #endif
