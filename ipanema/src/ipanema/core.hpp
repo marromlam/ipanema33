617f63d945ab4325ceb4850c558609f4512bedcb
Marcos Romero Lamas <marcos.romero.lamas@cern.ch>
2020-12-09 23:39:24 +0100

    separate header from main file for core

--- ipanema/src/ipanema/core.hpp
+++ ipanema/src/ipanema/core.hpp
@@ -1,23 +1,6 @@
 #ifdef USE_DOUBLE
   #define ftype double
   #define ctype double2
-  #ifndef CUDA
-  #pragma OPENCL EXTENSION cl_khr_int64_base_atomics: enable
-    WITHIN_KERNEL
-    void atomicAdd(volatile __global double *addr, double val)
-    {
-      union {
-        long u;
-        double f;
-      } next, expected, current;
-      current.f = *addr;
-      do {
-        expected.f = current.f;
-        next.f = expected.f + val;
-        current.u = atomic_cmpxchg( (volatile __global long *) addr, expected.u, next.u);
-      } while( current.u != expected.u );
-    }
-  #endif
 #else
   #define ftype float
   #define ctype float2
